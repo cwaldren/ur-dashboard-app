6da074789e1d09d2303ea8629b2ab1c900a46ef5
cwaldren <casey@fzy.me>
2014-03-24 00:35:38 -0400

    add some UI

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
-angular.module('dashboardApp', []);
+angular.module('dashboardApp', ['ngRoute'])
+
+.controller('WalletController', function($scope) {
+
+})
+
+.controller("LoginController", function($scope, $location, dashStorage) {
+    $scope.isStudent = dashStorage.get('isStudent');
+
+    $scope.proceedToLogin = function() {
+      dashStorage.put('isStudent', 'yes');
+      $scope.isStudent = true;
+    }
+
+    $scope.uninstall = function() {
+      chrome.management.uninstallSelf();
+    }
+})
+
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/wallet', {
+      templateUrl: 'views/wallet.html', 
+      controller: 'WalletController'
+    })
+    .when('/', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginController'
+    })
+    .otherwise({redirectTo: '/'});
+})
+
+.factory('dashStorage', function() {
+
+    return {
+        get: function(key) {
+            var item = localStorage.getItem(key);
+            return item ? window.atob(item) : null;
+        },
+
+        put: function(key, value) {
+            localStorage.setItem(key, window.btoa(value));
+        }
+    }
+})
